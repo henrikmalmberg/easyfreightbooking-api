662d885458e2d6264b892088f513920bcd1e89c4
henrikmalmberg <henrik@malmberg.cc>
2025-08-07 12:57:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,16 @@ import pytz
 import holidays
 import json
 
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine)
+
+
 app = Flask(__name__)
 CORS(app, origins=[
     "https://easyfreightbooking.com",
